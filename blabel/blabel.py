afbe6ecd7805ef83e24a52eb60476216c1086711
Kuba Raczkowski <kuba.raczkowski@spectricity.com>
2021-01-18 10:46:03 +0100

    adding png output

--- blabel/blabel.py
+++ blabel/blabel.py
@@ -51,6 +51,39 @@ def write_pdf(html, target=None, base_url=None, extra_stylesheets=()):
     else:
         weasy_html.write_pdf(target, stylesheets=extra_stylesheets)
 
+def write_png(html, target=None, base_url=None, extra_stylesheets=()):
+    """Write the provided HTML in a PDF file.
+
+    Parameters
+    ----------
+    html
+      A HTML string
+
+    target
+      A PDF file path or file-like object, or None for returning the raw bytes
+      of the PDF.
+
+    base_url
+      The base path from which relative paths in the HTML template start.
+
+    use_default_styling
+      Setting this parameter to False, your PDF will have no styling at all by
+      default. This means no Semantic UI, which can speed up the rendering.
+
+    extra_stylesheets
+      List of paths to other ".css" files used to define new styles or
+      overwrite default styles.
+    """
+    weasy_html = HTML(string=html, base_url=base_url)
+    if target in [None, "@memory"]:
+        with BytesIO() as buffer:
+            weasy_html.write_png(buffer, stylesheets=extra_stylesheets, resolution=96*4)
+            pdf_data = buffer.getvalue()
+        return pdf_data
+    else:
+        weasy_html.write_png(target, stylesheets=extra_stylesheets, resolution=96*4)
+
+
 class LabelWriter:
     """Class to write labels
 
@@ -145,6 +178,15 @@ class LabelWriter:
           document getting printed.
         """
         return write_pdf(
+            self.records_to_html(records),
+            target=target,
+            extra_stylesheets=self.default_stylesheets + extra_stylesheets,
+            base_url=base_url if base_url else self.default_base_url,
+        )
+
+    def write_png_labels(self, records, target=None, extra_stylesheets=(),
+                     base_url=None):
+        return write_png(
             self.records_to_html(records),
             target=target,
             extra_stylesheets=self.default_stylesheets + extra_stylesheets,
